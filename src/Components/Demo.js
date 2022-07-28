0f19510f28925003202d9830242c30e861c4672e
Aradhana135 <aradhana.kumari@creolestudios.com>
2022-07-28 18:05:50 +0530

    performdemo2

---
+++ src/Components/Demo.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+export default function Demo() {
+  const [isPlayerA, setIsPlayerA] = useState(true);
+  return (
+    <div>
+      {isPlayerA && <Counter person="Aradhana" />}
+      {!isPlayerA && <Counter person="XYZ" />}
+      <button
+        onClick={() => {
+          setIsPlayerA(!isPlayerA);
+        }}
+      >
+        Next player!
+      </button>
+    </div>
+  );
+}
+
+function Counter({ person }) {
+  const [score, setScore] = useState(0);
+  const [hover, setHover] = useState(false);
+
+  let className = "counter";
+  if (hover) {
+    className += " hover";
+  }
+
+  return (
+    <div
+      className={className}
+      onPointerEnter={() => setHover(true)}
+      onPointerLeave={() => setHover(false)}
+    >
+      <h1>
+        {person}'s score: {score}
+      </h1>
+      <button onClick={() => setScore(score + 1)}>Add one</button>
+    </div>
+  );
+}
